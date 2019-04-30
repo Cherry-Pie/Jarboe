d2883a1fb81bc6c31535b0e12d79c967dac2b986
Yaro <12fcv4@gmail.com>
2019-04-30 16:31:37 +0300

    extra display of errors

--- src/resources/views/crud/edit.blade.php
+++ src/resources/views/crud/edit.blade.php
@@ -10,15 +10,6 @@
 @endsection
 
 @section('content')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
 
     <!-- widget grid -->
     <section id="widget-grid" class="">
