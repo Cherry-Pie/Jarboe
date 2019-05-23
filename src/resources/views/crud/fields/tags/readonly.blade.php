9186b34a69f19ff54278d3fb49935ed7c2c6d524
Yaro <12fcv4@gmail.com>
2019-05-23 21:40:24 +0300

    fix tags field readonly state

--- src/resources/views/crud/fields/tags/readonly.blade.php
+++ src/resources/views/crud/fields/tags/readonly.blade.php
@@ -1,16 +1,29 @@
 
 <label class="label">{{ $field->title() }}</label>
-<label class="input state-disabled">
-    @if ($field->hasTooltip())
-        <i class="icon-append fa fa-question-circle"></i>
-    @endif
+<label class="input {{ $errors->has($field->name()) ? 'state-error' : '' }}">
 
-    <input type="text" value="{{ $model->{$field->name()} }}" disabled="disabled" placeholder="{{ $field->getPlaceholder() }}">
+    <select class="select-2--tags form-control" multiple="multiple" readonly>
+        @if ($field->isOptionsHidden())
+            @foreach ($field->getSelectedOptions($model) as $id => $value)
+                <option value="{{ $value }}" selected>{{ $value }}</option>
+            @endforeach
+        @else
+            @foreach ($field->getOptions() as $id => $value)
+                <option value="{{ $value }}" {{ $field->isCurrentOption($value, $model) ? 'selected' : '' }}>{{ $value }}</option>
+            @endforeach
+        @endif
+    </select>
 
-    @if ($field->hasTooltip())
-        <b class="tooltip tooltip-top-right">
-            <i class="fa fa-warning txt-color-teal"></i>
-            {{ $field->getTooltip() }}
-        </b>
-    @endif
 </label>
+
+
+@foreach ($errors->get($field->name()) as $message)
+    <div class="note note-error">{{ $message }}</div>
+@endforeach
+
+@pushonce('script_files', <script>
+  $(".select-2--tags").select2({
+    tags: true,
+    disabled: true
+  });
+</script>)
