a0acbbe5664504f04ed7a2552209e2a909cf7339
Yaro <12fcv4@gmail.com>
2019-06-05 09:46:00 +0300

    add image expander for image field

--- src/resources/views/crud/fields/image/inc/image_preview.blade.php
+++ src/resources/views/crud/fields/image/inc/image_preview.blade.php
@@ -26,15 +26,15 @@
                 }
 
                 $imageWrapper.append(
-                    '<div>' +
+                    '<div class="img-box">' +
                     '<img class="image-cropp-{{ $field->name() }}-'+ index +'" style="width: 100%;" />' +
                         @if ($field->isCrop())
                             '<input type="hidden" name="__{{ $field->name() }}_cropvalues['+ index +'][width]" class="{{ $field->name() }}_cropvalues_width_'+ index +'" />' +
-                    '<input type="hidden" name="__{{ $field->name() }}_cropvalues['+ index +'][height]" class="{{ $field->name() }}_cropvalues_height_'+ index +'" />' +
-                    '<input type="hidden" name="__{{ $field->name() }}_cropvalues['+ index +'][x]" class="{{ $field->name() }}_cropvalues_x_'+ index +'" />' +
-                    '<input type="hidden" name="__{{ $field->name() }}_cropvalues['+ index +'][y]" class="{{ $field->name() }}_cropvalues_y_'+ index +'" />' +
+                            '<input type="hidden" name="__{{ $field->name() }}_cropvalues['+ index +'][height]" class="{{ $field->name() }}_cropvalues_height_'+ index +'" />' +
+                            '<input type="hidden" name="__{{ $field->name() }}_cropvalues['+ index +'][x]" class="{{ $field->name() }}_cropvalues_x_'+ index +'" />' +
+                            '<input type="hidden" name="__{{ $field->name() }}_cropvalues['+ index +'][y]" class="{{ $field->name() }}_cropvalues_y_'+ index +'" />' +
                         @endif
-                            '</div>'
+                    '</div>'
                 );
                 $imageWrapper.append(document.createElement('hr'));
 
@@ -44,19 +44,19 @@
                 };
                 reader.readAsDataURL(file);
 
-                        @if ($field->isCrop())
-                var options{{ $field->name() }} = {
-                            @if ($field->getRatio('width') && $field->getRatio('height'))
-                            aspectRatio: {{ $field->getRatio('width') }} / {{ $field->getRatio('height') }},
-                            @endif
-                            viewMode: 2,
-                        crop: function(event) {
-                            $('.{{ $field->name() }}_cropvalues_width_'+ index).val(event.detail.width);
-                            $('.{{ $field->name() }}_cropvalues_height_'+ index).val(event.detail.height);
-                            $('.{{ $field->name() }}_cropvalues_x_'+ index).val(event.detail.x);
-                            $('.{{ $field->name() }}_cropvalues_y_'+ index).val(event.detail.y);
-                        }
-                    };
+                @if ($field->isCrop())
+                    var options{{ $field->name() }} = {
+                    @if ($field->getRatio('width') && $field->getRatio('height'))
+                        aspectRatio: {{ $field->getRatio('width') }} / {{ $field->getRatio('height') }},
+                    @endif
+                    viewMode: 2,
+                    crop: function(event) {
+                        $('.{{ $field->name() }}_cropvalues_width_'+ index).val(event.detail.width);
+                        $('.{{ $field->name() }}_cropvalues_height_'+ index).val(event.detail.height);
+                        $('.{{ $field->name() }}_cropvalues_x_'+ index).val(event.detail.x);
+                        $('.{{ $field->name() }}_cropvalues_y_'+ index).val(event.detail.y);
+                    }
+                };
                 $('.image-cropp-{{ $field->name() }}-'+ index).cropper(options{{ $field->name() }});
 
                 if ($('.image-cropp-{{ $field->name() }}-'+ index).data('cropper')) {
--- src/resources/views/crud/fields/image/inc/images_box.blade.php
+++ src/resources/views/crud/fields/image/inc/images_box.blade.php
@@ -2,18 +2,18 @@
     @if ($field->isMultiple())
         @foreach (($field->getUrl($model) ?: []) as $url)
             @if (filter_var($url, FILTER_VALIDATE_URL))
-                <div><img src="{{ $url }}" class="image-cropp-{{ $field->name() }}-{{ $loop->index }}" style="width: 100%;"></div>
+                <div class="img-box"><img src="{{ $url }}" class="image-cropp-{{ $field->name() }}-{{ $loop->index }}" style="width: 100%;"></div>
             @elseif (preg_match('~^data:~', $url))
-                <div><img src="{{ $url }}" class="image-cropp-{{ $field->name() }}-{{ $loop->index }}" style="width: 100%;"></div>
+                <div class="img-box"><img src="{{ $url }}" class="image-cropp-{{ $field->name() }}-{{ $loop->index }}" style="width: 100%;"></div>
             @else
                 <div>{{ $url }}</div>
             @endif
         @endforeach
     @else
         @if (filter_var($field->getUrl($model), FILTER_VALIDATE_URL))
-            <div><img src="{{ $field->getUrl($model) }}" class="image-cropp-{{ $field->name() }}-0" style="width: 100%;"></div>
+            <div class="img-box"><img src="{{ $field->getUrl($model) }}" class="image-cropp-{{ $field->name() }}-0" style="width: 100%;"></div>
         @elseif (preg_match('~^data:~', $field->getUrl($model)))
-            <div><img src="{{ $field->getUrl($model) }}" class="image-cropp-{{ $field->name() }}-0" style="width: 100%;"></div>
+            <div class="img-box"><img src="{{ $field->getUrl($model) }}" class="image-cropp-{{ $field->name() }}-0" style="width: 100%;"></div>
         @else
             <div>{{ $field->getUrl($model) }}</div>
         @endif
@@ -27,4 +27,22 @@
     div.wrapper-image-cropp > div {
         margin-bottom: 4px;
     }
+
+    div.wrapper-image-cropp > div.img-box {
+        max-height: 125px;
+        overflow: hidden;
+        -webkit-transition: max-height 1s;
+        -moz-transition: max-height 1s;
+        transition: max-height 1s;
+        cursor: pointer;
+    }
+    div.wrapper-image-cropp > div.img-box.expanded {
+        max-height: 999px;
+    }
 </style>)
+
+@pushonce('script_files', <script>
+    $(document).on('click', 'div.wrapper-image-cropp div.img-box', function() {
+        $(this).toggleClass('expanded');
+    });
+</script>)
